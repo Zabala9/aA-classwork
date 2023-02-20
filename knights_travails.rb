aa2f02a910e5592d419282412a993ded147d2aa2
Steven Zabala <zabalasteven9@gmail.com>
2023-02-20 15:06:49 -0800

    Adding methods

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,17 +1,51 @@
 require_relative "tree_node.rb"
 
 class KnightPathFinder
+    attr_reader :start_pos, :considered_positions, :root_node
 
     def initialize(start_pos)
         @start_pos = start_pos
         @root_node = PolyTreeNode.new(start_pos)
-        @considered_positions =
+        @considered_positions = [start_pos]
         self.build_move_tree
     end
 
-    # def self.valid_moves(pos)
+    def build_move_tree
+        queue = [@root_node]
+        until queue.empty?
+            node = queue.shift
+            new_move_positions(node.value).each do |pos|
+                child = PolyTreeNode.new(pos)
+                node.add_child(child)
+                queue << child
+            end
+        end
+    end
 
-    # end
+    def new_move_positions(pos)
+        # x, y = pos
+        moves = pos
+        moves.reject!{ |move| @considered_positions.include?(move) }
+        @considered_positions << moves
+        
+        moves
+    end
+
+    def self.valid_moves(pos)
+        x, y = pos
+        moves = [
+            [x+2, y+1],
+            [x+2, y-1],
+            [x-1, y+1],
+            [x-2, y-1],
+            [x+1, y+2],
+            [x+1, y-2],
+            [x-1, y+2],
+            [x-2, y+1]
+        ]
+
+        moves.select { |move| move.all?{ |cor| cor.between?(0, 7) } }
+    end
 
 # this is a test comment
 end
