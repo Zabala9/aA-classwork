57d1b5b871cc94b7d104aca419a88bac6749c857
Steven Zabala <zabalasteven9@gmail.com>
2023-02-20 15:52:00 -0800

    Adding find_path method to my knights_travails file

--- knights_travails.rb
+++ knights_travails.rb
@@ -7,7 +7,7 @@ class KnightPathFinder
         @start_pos = start_pos
         @root_node = PolyTreeNode.new(start_pos)
         @considered_positions = [start_pos]
-        self.build_move_tree
+        # self.build_move_tree
     end
 
     def build_move_tree
@@ -23,28 +23,42 @@ class KnightPathFinder
     end
 
     def new_move_positions(pos)
-        # x, y = pos
-        moves = pos
-        moves.reject!{ |move| @considered_positions.include?(move) }
-        @considered_positions << moves
-        
-        moves
+        x, y = pos
+
+        def self.valid_moves(pos)
+            x, y = pos
+            moves = [
+                [x+2, y+1],
+                [x+2, y-1],
+                [x-1, y+1],
+                [x-2, y-1],
+                [x+1, y+2],
+                [x+1, y-2],
+                [x-1, y+2],
+                [x-2, y+1]
+            ]
+    
+            moves.select { |move| move.all?{ |cor| cor.between?(0, 7) } }
+        end
+        # moves = pos
+        if valid_moves(pos)
+            # pos.select { |move| !@considered_positions.include?(move) ? @considered_positions << pos :  }
+            @considered_positions << pos if !@considered_positions.include?(pos)
+                
+            # end
+        end
+
+        pos
     end
 
-    def self.valid_moves(pos)
-        x, y = pos
-        moves = [
-            [x+2, y+1],
-            [x+2, y-1],
-            [x-1, y+1],
-            [x-2, y-1],
-            [x+1, y+2],
-            [x+1, y-2],
-            [x-1, y+2],
-            [x-2, y+1]
-        ]
-
-        moves.select { |move| move.all?{ |cor| cor.between?(0, 7) } }
+    def find_path(pos)
+        if @considered_positions.include?(pos)
+            @considered_positions.each_with_index do |ele, idx|
+                if ele == pos
+                    return @considered_positions[0..idx]
+                end
+            end
+        end
     end
 
 # this is a test comment
