1128b0b17700f90f2a4a468fb61baef58ad389e1
Steven Zabala <zabalasteven9@gmail.com>
2023-02-20 16:49:55 -0800

    Adding trace_path_back method

--- knights_travails.rb
+++ knights_travails.rb
@@ -51,20 +51,31 @@ class KnightPathFinder
         pos
     end
 
-    def find_path(pos)
-        if @considered_positions.include?(pos)
-            @considered_positions.each_with_index do |ele, idx|
-                if ele == pos
-                    return @considered_positions[0..idx]
-                end
-            end
+    # def find_path(pos)
+    #     if @considered_positions.include?(pos)
+    #         @considered_positions.each_with_index do |ele, idx|
+    #             if ele == pos
+    #                 return @considered_positions[0..idx]
+    #             end
+    #         end
+    #     end
+    # end
+
+    def find_path(end_pos)  
+        end_node = @root_node.dfs(end_pos)
+        trace_path_back(end_node)
+    end 
+    
+    def trace_path_back(end_node)
+        path = []
+
+        current_node = end_node
+        until current_node.nil?
+            path << current_node.value
+            current_node = current_node.parent
         end
+
+        path.reverse
     end
-    
-    # def find_path(end_pos)  
-    #     end_node = @root_node.dfs(end_pos)
-    #     trace_path_back(end_node)
-    # end 
-    
 # this is a test comment
 end
